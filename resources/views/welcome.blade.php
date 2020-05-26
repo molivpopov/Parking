34ef44b2949bd11556c77eb5f8e12ab3430c53f5
molivpopov <mitkop@mail.bg>
2020-05-26 11:04:23 +0300

    new welcome file - make active link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,47 +39,47 @@
         </thead>
         <tbody>
         <tr>
-            <th scope="row">/space</th>
+            <th scope="row"><a href="/space">/space</a></th>
             <td>Проверка на брой свободни места на паркинга.</td>
             <td>връща брой свободни места</td>
         </tr>
         <tr>
-            <th scope="row">/bill/{parking_id}/{time?}</th>
-            <td>Проверка на текущо дължима сума на превозно средство с даден номер. Ако не се напише {time} – означава сега.</td>
+            <th scope="row"><a href="https://parking.prepress.bg/bill/fcb46c8a-9e78-11ea-873c-525400fb8652">/bill/{parking_id}/{time?}</a></th>
+            <td>Проверка на текущо дължима сума на превозно средство с даден номер. Ако не се напише <a href="https://www.php.net/manual/en/datetime.formats.relative.php">{time}</a> – означава сега.</td>
             <td>връща дължимата сума към момента {time}. Също така и пълна разбивка на смятаната сума.</td>
         </tr>
         <tr>
-            <th scope="row">/entry/{vehicle_id}</th>
+            <th scope="row"><a href="https://parking.prepress.bg/entry/d2e076f6-9e78-11ea-873c-525400fb8652">/entry/{vehicle_id}</a></th>
             <td>Вход в паркинга - регистрация на превозното средство (вход е възможен само за предварително регистрирани превозни средства)</td>
             <td> връща <b>parking_id</b> <br><b style="color: red"> (пазете този номер, няма откъде другъде да бъде взет)</b></td>
         </tr>
         <tr>
-            <th scope="row">/exit/{parking_id}</th>
+            <th scope="row"><a href="https://parking.prepress.bg/exit/fcb46c8a-9e78-11ea-873c-525400fb8652">/exit/{parking_id}</a></th>
             <td>Изход от паркинга - дерегистрация на превозното средство</td>
             <td> връща дължимата сума в същия формат като /bill</td>
         </tr>
         <tr>
-            <th scope="row">/register/{number}/{class}</th>
+            <th scope="row"><a href="https://parking.prepress.bg/register/CA8890BH/1">/register/{number}/{class}</a></th>
             <td>Регистрация на ново превозно средство</td>
             <td> връща <b>vehicle_id</b> – уникален номера при регистрирания нов автомобил <br><b style="color: red"> (пазете този номер, няма откъде другъде да бъде взет)</b></td>
         </tr>
         <tr>
-            <th scope="row">/discounts/{vehicle_id}/{discount_id}</th>
+            <th scope="row"><a href="https://parking.prepress.bg/discounts/d2e076f6-9e78-11ea-873c-525400fb8652/2">/discounts/{vehicle_id}/{discount_id}</a></th>
             <td>Въвеждане на отстъпки</td>
             <td> връща текущо ползваната отстъпка</td>
         </tr>
         <tr>
-            <th scope="row">/discounts/{vehicle_id}</th>
+            <th scope="row"><a href="https://parking.prepress.bg/discounts/d2e076f6-9e78-11ea-873c-525400fb8652">/discounts/{vehicle_id}</a></th>
             <td>Проверка на отстъпка</td>
             <td> връща текущо ползваната отстъпка</td>
         </tr>
         <tr>
-            <th scope="row">/classes</th>
+            <th scope="row"><a href="https://parking.prepress.bg/classes">/classes</a></th>
             <td>Проверка на наличните типове превозни средства</td>
             <td> връща имената на наличните типове превозни средства с прилежащите id-та</td>
         </tr>
         <tr>
-            <th scope="row">/discounts</th>
+            <th scope="row"><a href="https://parking.prepress.bg/discounts">/discounts</a></th>
             <td>Проверка на наличните видове отстъпки</td>
             <td>връща имената на наличните видове отстъпки с прилежащите id-та</td>
         </tr>
